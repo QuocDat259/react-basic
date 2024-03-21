d7b20913c785638f9af244842a938fc8c6d11dd5
QuocDat259 <cunktvn06@gmail.com>
2024-03-21 20:13:24 +0700

    first

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello world with React.js (Trieu Quoc Dat)
         </p>
         <a
           className="App-link"
